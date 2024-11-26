116f77a57f1b4b08cc9dbfdd101bf51abd230808
krithiga-28 <krithigathanajeyan@gmail.com>
2024-11-26 17:24:34 +0530

    Updated MongoDB connection

--- server.js
+++ server.js
@@ -6,12 +6,13 @@ dotenv.config();  // Loads environment variables from a .env file
 
 const app = express();
 app.use(express.json());  // Middleware to parse JSON data
-
+const dbUri="mongodb+srv://krithiga:2BrSUfyfo3JYLlb5@employeedetails.ruh3c.mongodb.net/?retryWrites=true&w=majority&appName=EmployeeDetails";
 // MongoDB connection
-const dbUri = process.env.DB_URI;  // Get connection string from .env file
-mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true })
+  // Get connection string from .env file
+mongoose.connect(dbUri)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log("Error connecting to MongoDB", err));
+
 
 // Example route
 app.post('/insertEmployee', (req, res) => {
